39b70200e74ec89262b13b5a3e871bf342860c91
Kairi KAWASAKI <kairi.kawasaki@nide.jp>
2016-06-24 15:24:15 +0900

    [change] Compare the difference between HEAD, if you selected only one commit
    - Have not tested on Windows
    - Error occurs if some files are deleted on Mac

--- zip-diff.sh
+++ zip-diff.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-git archive --format=zip --prefix=www/ $1 `git diff --name-only $1 $2` -o ../diff.zip
-git diff --name-status $2 $1 > ../filelist.txt
+if [ "$2" = "" ]; then
+  git archive --format=zip --prefix=www/ HEAD `git diff --name-only HEAD $1` -o ../diff.zip
+  git diff --name-status $1 HEAD > ../filelist.txt
+else
+  git archive --format=zip --prefix=www/ $1 `git diff --name-only $1 $2` -o ../diff.zip
+  git diff --name-status $2 $1 > ../filelist.txt
+fi
