fd6137f0274acd943c10c058a0e7899001a27add
Kairi KAWASAKI <kairi.kawasaki@nide.jp>
2016-11-28 17:28:08 +0900

    [modify] Modify the order of commands

--- zip-diff.sh
+++ zip-diff.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+
 if [ "$2" = "" ]; then
-  git archive --format=zip --prefix=www/ HEAD `git diff --name-only HEAD $1` -o ../diff.zip
-  git diff --name-status $1 HEAD > ../filelist.txt
+  git diff --name-status HEAD $1 > ../filelist.txt
+  git archive --format=zip --prefix=www/ HEAD `git diff --diff-filter=AMCR --name-only HEAD $1` -o ../diff.zip
 else
-  git archive --format=zip --prefix=www/ $1 `git diff --name-only $1 $2` -o ../diff.zip
   git diff --name-status $2 $1 > ../filelist.txt
+  git archive --format=zip --prefix=www/ $1 `git diff --diff-filter=AMCR --name-only $2 $1` -o ../diff.zip
 fi
